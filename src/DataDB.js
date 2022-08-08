3a981a270b3e28a8165ff65c9831017ec967aabf
wjdgus1122 <qodqodantk@gmail.com>
2022-08-09 00:08:36 +0900

    Main Logo edit

---
+++ src/DataDB.js
@@ -0,0 +1,44 @@
+export const Publishing = [
+  {
+    id: 0,
+    title: "서울식물원",
+    project: "Team Project",
+    date: "2022 5.3 ~ 2022 6.13",
+    program: "Visual Studio Code",
+    languages: "JavaScript / CSS / HTML5",
+    post: "https://hij00.github.io/seoul_garden_renewal/ ",
+  },
+];
+
+export const ReactProject = [
+  {
+    id: 0,
+    title: "Movie App",
+    project: "Solo Project",
+    date: "2022 7.7 ~ 2022 7.28",
+    program: "Visual Studio Code",
+    languages: "JavaScript / CSS / HTML5",
+    post: "https://wjdgus1122.github.io/movie_project",
+  },
+  // {
+  //     id : 1,
+  //     title : "Dyson Renewal",
+  //     project : "Team Project",
+  //     date : "2022 6.24 ~ 2022 8.12",
+  //     program : "Visual Studio Code",
+  //     languages : "JavaScript / CSS / HTML5",
+  //     post : ""
+  // },
+];
+
+export const ToyProject = [
+  {
+    id: 0,
+    title: "Login",
+    project: "Solo Project",
+    date: "2022 7.19 ~ 2022 7.22",
+    program: "Visual Studio Code",
+    languages: "JavaScript / CSS / HTML5",
+    post: "",
+  },
+];
--- src/style/Globalstyle.js
+++ src/style/Globalstyle.js
@@ -1,6 +1,13 @@
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 
+export const mainStyle = {
+  BgColor: "#F1F6F9",
+  logoColor: "#14274E",
+  boldColor: "#394867",
+  fontColor: "#9BA4B4",
+};
+
 export const Globalstyle = createGlobalStyle`
     ${reset}
     body{
